b2d4c0e9668922dd742ec62a9c13769c4a7b093b
ramesh <rameshgetit@gmail.com>
2025-05-31 21:48:31 +0200

    By using `apply false`, you are telling Gradle that this plugin version is available for subprojects to use, but you are not applying it to the root project itself. Your `shared/build.gradle.kts` can then apply it as it currently does

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -30,7 +30,7 @@ kotlin {
             // put your Multiplatform dependencies here
         }
         commonTest.dependencies {
-            implementation(libs.kotlin.test)
+//            implementation(libs.kotlin.test)
         }
     }
 }
