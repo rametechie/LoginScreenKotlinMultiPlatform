b2e2672471363433578676c2aac54b3d1e818b9e
ramesh <rameshgetit@gmail.com>
2025-05-31 22:50:44 +0200

    Added the ios app module

---
+++ IosApp/SimpleloginIOS/SimpleloginIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleloginIOSApp.swift
+//  IosApp
+//
+//  Created by Ramesh P on 31/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleloginIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IosApp/SimpleloginIOSTests/SimpleloginIOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleloginIOSTests.swift
+//  SimpleloginIOSTests
+//
+//  Created by Ramesh P on 31/05/25.
+//
+
+import Testing
+
+struct SimpleloginIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
