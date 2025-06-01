ae3904068704f3fa67b8262545eebe22058a14b6
ramesh <rameshgetit@gmail.com>
2025-06-01 10:29:59 +0200

    Gradle's Kotlin Multiplatform Native framework binaries, with a custom task to create the .xcframework.

--- IosApp/SimpleloginIOS/ContentView.swift
+++ IosApp/SimpleloginIOS/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Shared
 
 struct ContentView: View {
     var body: some View {
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -17,9 +17,9 @@ kotlin {
     }
     
     listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
+        iosX64("iosX64"),
+        iosArm64("iosArm64"),
+        iosSimulatorArm64("iosSimulatorArm64")
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "Shared"
@@ -29,9 +29,10 @@ kotlin {
     // Apply iOS deployment target to all native targets
     targets.withType<KotlinNativeTarget>().configureEach {
         binaries.withType<org.jetbrains.kotlin.gradle.plugin.mpp.Framework>().configureEach {
-            freeCompilerArgs += "-Xapple.deployment-target=$iosDeploymentTarget"
+
         }
     }
+
     sourceSets {
         commonMain.dependencies {
             // put your Multiplatform dependencies here
@@ -39,6 +40,42 @@ kotlin {
         commonTest.dependencies {
 //            implementation(libs.kotlin.test)
         }
+    }
+
+    tasks.register("assembleDebugAppleFrameworkForXcode") {
+        dependsOn("linkDebugFrameworkIosArm64")
+    }
+
+    targets.withType<KotlinNativeTarget>().configureEach {
+        binaries.withType<org.jetbrains.kotlin.gradle.plugin.mpp.Framework>().configureEach {
+            freeCompilerArgs += "-Xapple.deployment-target=$iosDeploymentTarget"
+        }
+    }
+
+    val buildType = "debugFramework"
+    val frameworkName = "Shared"
+
+    tasks.register<Exec>("assembleXCFramework") {
+        group = "build"
+        description = "Create XCFramework manually"
+
+        dependsOn(
+            ":shared:linkDebugFrameworkIosArm64",
+            ":shared:linkDebugFrameworkIosX64",
+            ":shared:linkDebugFrameworkIosSimulatorArm64"
+        )
+
+
+        val outputDir = layout.buildDirectory.get().asFile.resolve("xcframework")
+        val arm64 = buildDir.resolve("bin/iosArm64/$buildType/$frameworkName.framework")
+        val simArm64 = buildDir.resolve("bin/iosSimulatorArm64/$buildType/$frameworkName.framework")
+
+        commandLine(
+            "xcodebuild", "-create-xcframework",
+            "-framework", arm64.absolutePath,
+            "-framework", simArm64.absolutePath,
+            "-output", outputDir.resolve("$frameworkName.xcframework").absolutePath
+        )
     }
 }
 
